1ba0075f1c9bc2b155ca8983206b8e1412b7addf
jonny2402 <72766718+jonny2402@users.noreply.github.com>
2021-10-19 17:23:25 +0100

    Add files via upload

--- PebbleGame.java
+++ PebbleGame.java
@@ -1,96 +1,101 @@
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class PebbleGame {
-    private static int number_of_players;
-    private static ArrayList < Players > playerList = new ArrayList < Players > ();
-    private ArrayList<Bags> bagList; //change variable
-    private static String file_location_0 = "";
-    private static String file_location_1 = "";
-    private static String file_location_2 = "";
+  private Int number_of_players;
+  private ArrayList<Players> playerList;
+  private ArrayList<Bags> bagList;
+  private String file_location_0;
+  private String file_location_1;
+  private String file_location_2;
+  // maybe change to individual variables
+  private Boolean[] valid_array;
 
-  public PebbleGame() {
+  // CONSTRUCTOR METHOD FOR CARDGAME CLASS
+  public CardGame() {
     playerList = new ArrayList<Players>();
     // change from arraylist to array
-    bagList = new ArrayList<Bags>();
+    bagList = new arrayList<Bags>();
   }
   public static void main(String[] args) {
-      System.out.println("Welcome to the PebbleGame!!");
-      System.out.println("You will be asked to enter the number of players and then then for the " +
-              "location of three files in turn containing comma separated integer values for the " +
-              "pebble weights.");
-      System.out.println("The integer values must be strictly positive.");
-      System.out.println("The game will be simulated, and output written to files in this directory.");
-      userInput();
-      if (PebbleGame.testUserInputPlayer(number_of_players)) {
-          if (testUserInputLocation(file_location_0)
-                  && testUserInputLocation(file_location_1)
-                  && testUserInputLocation(file_location_2)) {
-              // test file formatting here
-              try {
-                  // convertFileToArrayList(file_location_0); // need to convert all at once
-                  createPlayers(number_of_players);
-                  Bags.dealToBags();
-              } catch (Exception e) {
-                  System.out.println("There was an error with the file that you submitted.");
-              }
-          }
-          //SUCCESS MESSAGE
-      }
-  }
+    // CALL FOR USER TO INPUT
+    userInput();
+    testInputs(number_of_players, file_location_0, file_location_1, file_location_2);
+    // LOOP THROUGH VALID_ARRAY TO TELL THE USER WHETHER FILE LOCATIONS ARE VALID
+    // EXCEPT VALID_ARRAY[0] - (PLAYERS VALID)
+    for (int i = 1; i < valid_array.length; i++) {
+      // PLAYER NUMBER SUCCESS MESSAGE
+      System.out.println("Testing number of players...");
+      if (valid_array[0] == True) {
+        System.out.println("Success!")
+      } else {System.out.println("Failure. Please choose a valid number of players.")}
+      // FILE LOADING SUCCESS MESSAGES
+      System.out.println("Loading file " + i + "... : " );
+      if (valid_array[i] == true) {
+        System.out.println("Success!" );
+      } else {System.out.println("Failure.")
+    System.out.println("Please enter a valid location for file " + i)}
+    }
+    // AFTER VALIDITY CHECKS DONE, USE INPUTS
+    try {
+      // doesn't work yet
+      convertFileToArrayList(file_location);
+      createPlayers(number_of_players);
+      Bags.dealToBags();
+    }
+    catch (Exception e) {
+      System.out.println("There was an error with the file(s) that you submitted.");
+    }
+      //SUCCESS MESSAGE
+    }
 
-
-
-  // METHOD THAT RECEIVES USER INPUT IN COMMAND LINE
-  public static void userInput() {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Please enter the number of players: ");
-      while (!scanner.hasNextInt()) {
-        System.out.println("Please enter a valid integer.");
-        scanner.nextLine();
-      }
-      number_of_players = scanner.nextInt();
-      System.out.println("Please enter the location of bag number 0 to load: ");
-      file_location_0 = scanner.next();
-      System.out.println("Please enter the location of bag number 1 to load: ");
-      file_location_1 = scanner.next();
-      System.out.println("Please enter the location of bag number 2 to load: ");
-      file_location_2 = scanner.next();
-      scanner.close();
-  }
+  // METHOD THAT RECIEVES USER INPUT IN COMMAND LINE
+ public userInput() {
+   Scanner scanner = new scanner(System.in);
+   System.out.println("Please enter the number of players: ");
+   while (!scan.hasNextInt()) {
+     System.out.println("Please enter a valid integer.");
+     scan.nextLine();
+   }
+   number_of_players = scanner.nextInt();
+   System.out.println("Please enter the location of bag number 0 to load: ");
+   file_location_0 = scanner.next();
+   System.out.println("Please enter the location of bag number 1 to load: ");
+   file_location_1 = scanner.next();
+   System.out.println("Please enter the location of bag number 2 to load: ");
+   file_location_2 = scanner.next();
+ }
 
  // METHOD TO TEST WHETHER THE NUMBER OF PLAYER INPUT IS VALID
-  public static Boolean testUserInputPlayer(int number_of_players) {
+  public Boolean testUserInputPlayer(number_of_players) {
     //if (number_of_players instanceof Integer && number_of_players > 0) {
-      //return number_of_players > 0;
-      return number_of_players > 0;
+    if (number_of_players > 0) {
+      return True;
+    } else {return False;}
   }
 
   // METHOD TO TEST WHETHER THE FILE INPUT EXISTS AND IS IN LEGITIMATE FORMAT
-  public static boolean testUserInputLocation(String file_location) {
-     //return Files.exists(Path.of(file_location));
-      File temp = new File(file_location);
-      return temp.exists();
+  public Boolean testUserInputLocation(file_location) {
+      return Files.exists(file_location);
   }
-  // METHOD TO TEST FILE FORMATTING
-  public static boolean testFileFormat(String file_location) {
-      // complete
-      return true;
+
+  // METHOD TO CALL ALL QUERY METHODS TOGETHER
+  public void testInputs(Int number_of_players, String file_location_0, String file_location_1, String file_location_2) {
+    Boolean players_valid = testUserInputPlayer(number_of_players);
+    Boolean file_0_valid = testUserInputLocation(file_location_0);
+    Boolean file_1_valid = testUserInputLocation(file_location_1);
+    Boolean file_2_valid = testUserInputLocation(file_location_2);
+    valid_array = {players_valid, file_0_valid, file_1_valid, file_2_valid};
   }
+
+
 
   // METHOD TO CONVERT INPUT FILE INTO AN ARRAY LIST
-  public static ArrayList<Integer> convertFileToArrayList(File file) {
+  public arrayList<Integer> convertFileToArrayList(file) {
     // complete this
-      return null;
   }
-
-  public static void createPlayers(int number_of_players) {
-      for (int i = 1; i <= number_of_players; i++) {
+  // METHOD TO CREATE N NUMBER OF PLAYER SELECTED BY THE USER
+  public void createPlayers(int number_of_players) {
+    for (i = 0; i < number_of_players; i++) {
       // fill in players class attributes
-          playerList.add(new Players(1));
+      playerList.add(new Players());
     }
   }
 }
--- Players.java
+++ Players.java
@@ -1,22 +1,26 @@
-import java.util.ArrayList;
+
 
 public class Players {
-  // may be unnecessary
-  private int playerID;
+  private int playerID
   private ArrayList<Integer> playerInventory;
   // CONSTRUCTOR METHOD
   public Players(int playerID) {
-    this.playerID = playerID;
+    this.playerID = playerID
   }
 
   // METHOD THAT RETURNS IF A PLAYER HAS WON
-  public boolean getPlayerWinState() {
+  public getPlayerWinState() {
     int InventoryTotal = 0;
     //this.playerInventory?
     for (int i: playerInventory) {
-      InventoryTotal += i;
+      InventoryTotal += i:
     }
-    return InventoryTotal == 100;
+    if (InventoryTotal == 100) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
 
